768666a6f362ddb506aebb930f93d90e661a61c0
TijanaDev <tijanailic933@gmail.com>
2021-01-17 00:33:16 +0100

    End of section 2

---
+++ API/Data/Migrations/20210116230817_InsertDataToUsers.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Data.Migrations
+{
+    public partial class InsertDataToUsers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                "INSERT INTO Users VALUES('Tijana')"
+            );
+                 migrationBuilder.Sql(
+                "INSERT INTO Users VALUES('Aca')"
+                
+            );
+
+             migrationBuilder.Sql(
+                "INSERT INTO Users VALUES('Vanja')"
+                
+            );
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                "DELETE FROM Users"
+            );
+        }
+    }
+}
